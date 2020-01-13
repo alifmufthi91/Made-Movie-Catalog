b2828558dcef83601b10e6e1ee66ba980822ab66
Alif Mufthi <alif.stinky@gmail.com>
2020-01-13 16:15:10 +0700

    first commit

---
+++ app/src/main/java/com/example/moviecatalogue_made_s2/SectionsPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.moviecatalogue_made_s2
+
+import android.content.Context
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+
+class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    private val TAB_TITLES = intArrayOf(
+        R.string.tab_text_1,
+        R.string.tab_text_2)
+    override fun getItem(position: Int): Fragment {
+        val fragment = ListFragment.newListPage(position + 1)
+        return fragment
+    }
+
+    @Nullable
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+}
